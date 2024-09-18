30471d3a3c969d9622a08c0145aad7cff98f6d86
Junayed-shanto <junayedshanto54@gmail.com>
2024-09-18 09:38:02 +0600

    edit staticfiles name file

--- quiz_portal/settings.py
+++ quiz_portal/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
