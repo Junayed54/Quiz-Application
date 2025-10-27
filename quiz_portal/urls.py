d8b93384087a136cb218a875623bf02615c4e585
Junayed <junayedshanto54@gmail.com>
2025-10-27 14:10:07 +0600

    added the app=ads.txt file

--- quiz_portal/urls.py
+++ quiz_portal/urls.py
@@ -35,6 +35,11 @@ urlpatterns = [
     
     path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     path("sitemap.xml", TemplateView.as_view(template_name="sitemap.xml", content_type="application/xml")),
+    path(
+        "app-ads.txt",
+        TemplateView.as_view(template_name="app-ads.txt", content_type="text/plain"),
+        name="app_ads_txt",
+    ),
 ]
 
 
